11d1d28aa868c90a52be19e86005f0f113be4a92
Karl Eklund <localpart@gmail.com>
2021-12-04 18:23:33 +0100

    read the input file

--- 4/src/main.lisp
+++ 4/src/main.lisp
@@ -2,4 +2,29 @@
   (:use :cl))
 (in-package :submarine-bingo)
 
-;; blah blah blah.
+;; From https://stackoverflow.com/a/59048247
+
+(defun chunked (seq size)
+  (let* ((total (length seq))
+         (amount (ceiling total size))
+         (res '()))
+    (dotimes (i amount res)
+      (push (subseq seq (* i size) (min (* (1+ i) size) total))
+            res))))
+
+(defun join-lines-with-space (lines)
+  (eval (append '(concatenate 'string)
+                (mapcar (lambda (number) (concatenate 'string number " "))
+                        lines))))
+
+(let*
+    ((input-file-path (asdf:system-relative-pathname :submarine-bingo "../input"))
+     (input (uiop:read-file-string input-file-path))
+     (lines (uiop:split-string input :separator '(#\Newline)))
+     (called-numbers (car lines))
+     (board-lines (cdr lines))
+     (board-numbers-text (join-lines-with-space board-lines))
+     (board-numbers-sexp (concatenate 'string "(" board-numbers-text ")"))
+     (board-numbers (read-from-string board-numbers-sexp))
+     (boards (chunked board-numbers 25)))
+ boards)
