b6d6778e9e8093bf8a229bbcf8573bb75384e731
Karl Eklund <localpart@gmail.com>
2021-12-05 19:28:11 +0100

    add project for day 5

---
+++ 5/hydrothermalventure/src/se/karleklund/hydrothermalventure/Main.java
@@ -0,0 +1,8 @@
+package se.karleklund.hydrothermalventure;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("At how many points do at least two lines overlap?");
+    }
+}
