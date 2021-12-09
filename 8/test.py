1f5c169a0858ab53ddd1297c929b22ff4038fe4b
Karl Eklund <localpart@gmail.com>
2021-12-09 21:16:04 +0100

    calculate all the digits

--- 8/test.py
+++ 8/test.py
@@ -7,10 +7,10 @@ f = open("example")
 lines = [line.rstrip() for line in f]
 
 
-class EntryTest(unittest.TestCase):
-    def test_create_entry(self):
-        entry = Entry(lines[0])
-        self.assertEqual(Digit, type(entry.at(0)))
+# class EntryTest(unittest.TestCase):
+#     def test_create_entry(self):
+#         entry = Entry(lines[0])
+#         self.assertEqual(Digit, type(entry.at(0)))
 
 
 class PatternTest(unittest.TestCase):
@@ -18,13 +18,35 @@ class PatternTest(unittest.TestCase):
         "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")
 
     def test_fixed_number_of_segments(self):
+        print("test fixed number of segments")
         self.assertEqual(1, self.entry.at(9).value)
         self.assertEqual(4, self.entry.at(7).value)
         self.assertEqual(7, self.entry.at(4).value)
         self.assertEqual(8, self.entry.at(0).value)
 
     def test_3(self):
+        print("test 3")
         self.assertEqual(3, self.entry.at(3).value)
+
+    def test_0(self):
+        print("test 0")
+        self.assertEqual(0, self.entry.at(8).value)
+
+    def test_2(self):
+        print("test 2")
+        self.assertEqual(2, self.entry.at(2).value)
+
+    def test_9(self):
+        print("test 9")
+        self.assertEqual(9, self.entry.at(5).value)
+
+    def test_6(self):
+        print("test 6")
+        self.assertEqual(6, self.entry.at(6).value)
+
+    def test_5(self):
+        print("test 5")
+        self.assertEqual(5, self.entry.at(1).value)
 
 
 if __name__ == '__main__':
